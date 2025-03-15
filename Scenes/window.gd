3acf1151ce6f4eb9940aac9e9c2b168b22f9977f
Alena <alena132@web.de>
2025-03-15 23:10:07 +0100

    fixed it manually

---
+++ Scenes/window.gd
@@ -0,0 +1,14 @@
+extends Window
+#{load MINESWEEPER IN THIS SOMEHOW res://Scripts/minesweeper_boardv2.gd 
+#
+#FUCK MOVING AND RESIZING ITS A FRIGGIN GAMEJAM
+#
+#onclick - open menu.tscn
+#(idk if we need to put it as current window, I assume this blocks minesweeper automatically)
+#
+#buttons 
+#1 - start game
+#2 - quit game
+
+#cat animation is done already
+#dialogue start
