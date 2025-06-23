2fbb37e95b93b5782af4cb3f5f05c3dbf9e50075
Bas Vegt <basvegt+github@gmail.com>
2025-06-23 21:36:25 +0200

    set up further work for tomorrow
    a resoruce manager is set up and i have the move function but i can't use it yet. sleepy time now so will continue tomorrow if time

--- Scripts/cursor_state_default.gd
+++ Scripts/cursor_state_default.gd
@@ -6,3 +6,16 @@ func on_state_entered() -> void:
 	# Called when the default state is entered
 	print("Default cursor state entered")
 	# Optionally set the cursor sprite or other properties here
+
+func on_input(event: InputEvent) -> void:
+	# Handle input events specific to the default cursor state
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			print("Mouse button pressed in default state")
+		else:
+			print("Mouse button released in default state")
+	elif event is InputEventMouseMotion:
+		print("Mouse moved in default state")
+		#TODO move cursor location
+		# but to do that you need to affect the node
+		# so you need to give that in the function, if you do it this way
--- Scripts/resource_manager.gd
+++ Scripts/resource_manager.gd
@@ -1,2 +1,4 @@
 extends Node
 
+@export var cursor_sprite_default: Texture2D
+@export var cursor_sprite_laser: Image
